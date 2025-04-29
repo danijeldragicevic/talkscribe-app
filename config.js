ea67377f02b1ede4bacaf36dc08aa793c55d2f82
Danijel Dragicevic <danijel.dragicevic@productdock.com>
2025-04-29 10:26:32 +0200

    Switch to the API GW link.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    BASE_URL: "http://localhost:8080/api"
-    //BASE_URL: "https://d98jbs0ixj.execute-api.eu-central-1.amazonaws.com/dev/api"
+    //BASE_URL: "http://localhost:8080/api"
+    BASE_URL: "https://d98jbs0ixj.execute-api.eu-central-1.amazonaws.com/dev/api"
 };
