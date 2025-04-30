7f827066d0be9f504cad3e219cf3ce5cf0c70701
Danijel Dragicevic <danijel.dragicevic@productdock.com>
2025-04-30 10:39:31 +0200

    Revert "Handle http 429 from the backend."
    This reverts commit 1133df42d628b7de93f7d7237cc11df7d048ff16.

--- script.js
+++ script.js
@@ -77,17 +77,7 @@ function sendTextToSpeech() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: text })
     })
-        .then(res => {
-            if (res.ok) {
-                return res.blob();
-            } else if (res.status === 429) {
-                return res.json().then(data => {
-                    throw new Error(data.error || "Too many requests");
-                });
-            } else {
-                return Promise.reject(res);
-            }
-        })
+        .then(res => res.ok ? res.blob() : Promise.reject(res))
         .then(blob => {
             const url = URL.createObjectURL(blob);
             audioPlayer.src = url;
@@ -95,14 +85,10 @@ function sendTextToSpeech() {
             audioPlayer.play();
         })
         .catch(err => {
-            if (err.message === "Too many requests") {
-                alert("You are sending too many requests. Please wait a moment and try again.");
-            } else {
-                console.error("TTS error:", err);
-                audioPlayer.src = "error_message.mp3";
-                audioContainer.style.display = "block";
-                audioPlayer.play();
-            }
+            console.error("TTS error:", err);
+            audioPlayer.src = "error_message.mp3";
+            audioContainer.style.display = "block";
+            audioPlayer.play();
         });
 }
 
